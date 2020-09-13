020a9459f6ef476bc6b721f368da38ae19da6748
Ablard <abelardfakoundo@yahoo.fr>
2020-09-14 01:31:34 +0200

    Add: install and open Library

--- first.r
+++ first.r
@@ -1,1 +1,12 @@
-va1=2*2
+
+
+#Install package(library) with this line: 
+
+install.packages("ggplot2")
+#open a library with library() or require() both are same:
+
+library(ggplot2)
+require(ggplot2)
+
+
+
